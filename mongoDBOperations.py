79a0ae22ed40cda6f9819f0b49c997bfbd2e8daf
Soumalla-Tarafder <91072267+Soumalla-Tarafder@users.noreply.github.com>
2021-12-22 16:48:44 +0530

    Update mongoDBOperations.py

--- mongoDBOperations.py
+++ mongoDBOperations.py
@@ -12,7 +12,7 @@ class MongoDBManagement:
         try:
             self.username = username
             self.password = password
-            self.url = "mongodb+srv://ineuron:ineuron@cluster1.ypvyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(
+            self.url = "mongodb+srv://soumalla1:12345@cluster0.028f1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(
                 self.username, self.password)
             # self.url = 'localhost:27017'
         except Exception as e:
